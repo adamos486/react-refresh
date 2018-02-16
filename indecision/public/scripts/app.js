38e2311512f5f0bbeab81dc2d62e513a5bbfa3d1
Adam Cobb <adamos486@gmail.com>
2018-02-15 16:15:07 -0800

    cleaning up some boilerplate and earlier examples.

--- indecision/public/scripts/app.js
+++ indecision/public/scripts/app.js
@@ -2,103 +2,11 @@
 
 console.log("App.js is running");
 
-var user = {
-  name: "Adam Cobb",
-  age: 30,
-  location: "San Francisco"
-};
-
 //only render the subtitle if exists. logical and
 //create a brand new property: options, array of string options.
 
-var displayChunk = {
-  title: "Some annoying title",
-  subTitle: "Some annoying other title",
-  options: ["Properties", "Sales", "Escrow", "Underwriting"]
-};
-
-function populateOptions(options) {
-  return options.map(function (option) {
-    return React.createElement(
-      "li",
-      {
-        style: {
-          cursor: "pointer"
-        },
-        key: option,
-        onClick: function onClick(e) {
-          e.preventDefault();
-          clickOption(option);
-        }
-      },
-      option
-    );
-  });
-}
-
 function clickOption(option) {
   console.log("Option clicked:", option);
-}
-
-function checkForOptions(options) {
-  if (options && options.length > 0) {
-    return React.createElement(
-      "div",
-      null,
-      React.createElement(
-        "h4",
-        null,
-        "Here are your options:"
-      ),
-      React.createElement(
-        "ul",
-        null,
-        populateOptions(options)
-      )
-    );
-  }
-}
-
-function checkSubtitle(subtitle) {
-  if (subtitle && subtitle.length > 0) {
-    return React.createElement(
-      "p",
-      null,
-      subtitle
-    );
-  }
-}
-
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      "p",
-      null,
-      "Location: ",
-      location
-    );
-  }
-}
-
-function getName(name) {
-  if (name) {
-    return React.createElement(
-      "h1",
-      null,
-      name
-    );
-  }
-}
-
-function getAge(age) {
-  if (age) {
-    return React.createElement(
-      "p",
-      null,
-      "Age: ",
-      age
-    );
-  }
 }
 
 var plusId = 100;
@@ -109,58 +17,51 @@ var count = 0;
 var addOne = function addOne() {
   console.log("addOne()");
   count++;
+  renderCounterApp();
 };
 
 var minusOne = function minusOne() {
   console.log("minusOne()");
   count--;
+  renderCounterApp();
 };
 
 var reset = function reset() {
   console.log("reset");
   count = 0;
+  renderCounterApp();
 };
-
-var counterTemplate = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    "Count: ",
-    count
-  ),
-  React.createElement(
-    "button",
-    { id: plusId, onClick: addOne },
-    "+1"
-  ),
-  React.createElement(
-    "button",
-    { id: minusId, className: "button", onClick: minusOne },
-    "-1"
-  ),
-  React.createElement(
-    "button",
-    { id: resetId, className: "button", onClick: reset },
-    "Reset"
-  )
-);
-console.log(counterTemplate);
-
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    displayChunk.title
-  ),
-  checkSubtitle(displayChunk.subTitle),
-  checkForOptions(displayChunk.options),
-  counterTemplate
-);
 
 var app = document.getElementById("app");
 
-ReactDOM.render(template, app);
+var renderCounterApp = function renderCounterApp() {
+  var counterTemplate = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      "Count: ",
+      count
+    ),
+    React.createElement(
+      "button",
+      { id: plusId, onClick: addOne },
+      "+1"
+    ),
+    React.createElement(
+      "button",
+      { id: minusId, className: "button", onClick: minusOne },
+      "-1"
+    ),
+    React.createElement(
+      "button",
+      { id: resetId, className: "button", onClick: reset },
+      "Reset"
+    )
+  );
+
+  ReactDOM.render(counterTemplate, app);
+};
+
+renderCounterApp();
--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,76 +1,10 @@
 console.log("App.js is running");
 
-const user = {
-  name: "Adam Cobb",
-  age: 30,
-  location: "San Francisco"
-};
-
 //only render the subtitle if exists. logical and
 //create a brand new property: options, array of string options.
 
-const displayChunk = {
-  title: "Some annoying title",
-  subTitle: "Some annoying other title",
-  options: ["Properties", "Sales", "Escrow", "Underwriting"]
-};
-
-function populateOptions(options) {
-  return options.map(option => {
-    return (
-      <li
-        style={{
-          cursor: "pointer"
-        }}
-        key={option}
-        onClick={e => {
-          e.preventDefault();
-          clickOption(option);
-        }}
-      >
-        {option}
-      </li>
-    );
-  });
-}
-
 function clickOption(option) {
   console.log("Option clicked:", option);
-}
-
-function checkForOptions(options) {
-  if (options && options.length > 0) {
-    return (
-      <div>
-        <h4>Here are your options:</h4>
-        <ul>{populateOptions(options)}</ul>
-      </div>
-    );
-  }
-}
-
-function checkSubtitle(subtitle) {
-  if (subtitle && subtitle.length > 0) {
-    return <p>{subtitle}</p>;
-  }
-}
-
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  }
-}
-
-function getName(name) {
-  if (name) {
-    return <h1>{name}</h1>;
-  }
-}
-
-function getAge(age) {
-  if (age) {
-    return <p>Age: {age}</p>;
-  }
 }
 
 const plusId = 100;
@@ -81,43 +15,40 @@ let count = 0;
 const addOne = () => {
   console.log("addOne()");
   count++;
+  renderCounterApp();
 };
 
 const minusOne = () => {
   console.log("minusOne()");
   count--;
+  renderCounterApp();
 };
 
 const reset = () => {
   console.log("reset");
   count = 0;
+  renderCounterApp();
 };
-
-const counterTemplate = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button id={plusId} onClick={addOne}>
-      +1
-    </button>
-    <button id={minusId} className="button" onClick={minusOne}>
-      -1
-    </button>
-    <button id={resetId} className="button" onClick={reset}>
-      Reset
-    </button>
-  </div>
-);
-console.log(counterTemplate);
-
-const template = (
-  <div>
-    <h1>{displayChunk.title}</h1>
-    {checkSubtitle(displayChunk.subTitle)}
-    {checkForOptions(displayChunk.options)}
-    {counterTemplate}
-  </div>
-);
 
 const app = document.getElementById("app");
 
-ReactDOM.render(template, app);
+const renderCounterApp = () => {
+  const counterTemplate = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button id={plusId} onClick={addOne}>
+        +1
+      </button>
+      <button id={minusId} className="button" onClick={minusOne}>
+        -1
+      </button>
+      <button id={resetId} className="button" onClick={reset}>
+        Reset
+      </button>
+    </div>
+  );
+
+  ReactDOM.render(counterTemplate, app);
+};
+
+renderCounterApp();
